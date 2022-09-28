006906bb3bd42f927fe01fcc1bff96e70093272c
nighoggDatatype <hosherman1965@gmail.com>
2022-09-28 22:00:43 +0800

    Adjust first bat script to match second script

--- win_build_script_A1.1.bat
+++ win_build_script_A1.1.bat
@@ -4,10 +4,8 @@ rem From now on, we need to have Docker daemon running
 docker build . -t nighogg-datatype-cs3244/node-web-app
 
 rem Note that we use the port 3000, as implied in the Assignment paper
-docker run -it -p 3000:8080 nighogg-datatype-cs3244/node-web-app 
-rem Exit the process by inputting CTRL-C
+docker run --name A1p1 -p 3000:8080 -d --rm nighogg-datatype-cs3244/node-web-app 
 
-rem Alternatively, we can run the container in detatched mode, but this requires manual stopping
-rem `docker run -p 3000:8080 -d nighogg-datatype-cs3244/node-web-app`
-rem //TODO: Outline in the report how to manually stop docker containers
-rem //      Probably either by manual commands, or by using "Docker Desktop"
+set /p DUMMY=Hit ENTER to stop the program
+
+docker container stop A1p1
